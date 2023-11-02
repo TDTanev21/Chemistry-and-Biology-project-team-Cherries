49e28b2e23a9d63ed73590f726bdc9d16509a6d4
TDTanev21 <TDTanev21@codingburgas.bg>
2023-11-02 22:55:29 +0200

    Create the game's background movement

--- Chemistry-and-biology-project/Chemistry-and-biology-project/Chemistry-and-biology-project.cpp
+++ Chemistry-and-biology-project/Chemistry-and-biology-project/Chemistry-and-biology-project.cpp
@@ -3,7 +3,40 @@
 using namespace std;
 void game()
 {
+    SetExitKey(KEY_ESCAPE);
+    
+    Texture2D ship;
+    ship = LoadTexture("../images/ship1.png");
+    Vector2 ballPosition = { -100.0f, -100.0f };
 
+    Texture2D bg = LoadTexture("../images/gamebg.png");
+    Rectangle bgRect[2] = { { 0, 0, bg.width, bg.height} , {0, -bg.height, bg.width, bg.height} };
+    float speed = 3;
+
+    while (!WindowShouldClose())
+    {
+        ballPosition = GetMousePosition();
+
+        BeginDrawing();
+
+        for (int i = 0; i < 2; i++)
+        {
+            bgRect[i].y += speed;
+            if ((bgRect[i].y) >= GetScreenHeight())
+                bgRect[i].y = -bg.height;
+
+            DrawTexture(bg, bgRect[i].x, bgRect[i].y, WHITE);
+        }
+        DrawFPS(50, 50);
+
+        DrawTexture(ship, GetScreenWidth() / 3 + 90, 700, WHITE);
+        
+        DrawCircleV(ballPosition, 10, BLACK);
+
+        HideCursor();
+        ClearBackground(BLUE);
+        EndDrawing();
+    }
 }
 void rulesFunction()
 {
@@ -12,27 +45,37 @@ void rulesFunction()
 float HEIGHT, WIDTH;
 int main()
 {
-    InitWindow(0, 0, "Eco Pirates");
+    WIDTH = GetScreenWidth();
+    HEIGHT = GetScreenHeight();
+    InitWindow(WIDTH, HEIGHT, "Eco Pirates");
     ToggleFullscreen();
 
     SetTargetFPS(60);
-    WIDTH = GetScreenWidth();
-    HEIGHT = GetScreenHeight();
-    Texture2D background, startGame, rules, quitGame;
+   
+    Texture2D background,startGame, rules, quitGame;
     Vector2 mousePoint;
     Vector2 ballPosition = { -100.0f, -100.0f };
     bool exitGame = false;
+    
+    
     background = LoadTexture("../images/menubg.png");
     startGame = LoadTexture("../images/start.png");
     rules = LoadTexture("../images/rules.png");
     quitGame = LoadTexture("../images/quit.png");
+    
+    
     Rectangle startGameButton = { 820, 350, startGame.width, startGame.height };
     Rectangle rulesButton = { 820, 475, rules.width, rules.height };
     Rectangle quitGameButton = { 820, 600, quitGame.width, quitGame.height };
+    
+    
+    
     while (!exitGame)
     {
         mousePoint = GetMousePosition();
         ballPosition = GetMousePosition();
+        
+        
         BeginDrawing();
         DrawFPS(50, 50);
 
