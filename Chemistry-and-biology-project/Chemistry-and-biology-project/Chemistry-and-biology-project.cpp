cbe840e68e65e207f08e29b331da606efab66c46
TDTanev21 <TDTanev21@codingburgas.bg>
2023-11-03 23:54:04 +0200

    Make the Rules menu

--- Chemistry-and-biology-project/Chemistry-and-biology-project/Chemistry-and-biology-project.cpp
+++ Chemistry-and-biology-project/Chemistry-and-biology-project/Chemistry-and-biology-project.cpp
@@ -45,7 +45,44 @@ void game()
 }
 void rulesFunction()
 {
+    SetExitKey(KEY_ESCAPE);
 
+    Vector2 ballPosition = { -100.0f, -100.0f };
+    Vector2 mousePoint = { -100.0f, -100.0f };
+    Texture2D exitToGame, exitToMenu;
+    exitToGame = LoadTexture("../images/rulesToGame.png");
+    exitToMenu = LoadTexture("../images/rulesToMenuButton.png");
+    Rectangle exitRulesButton = { 1635, 925, exitToGame.width, exitToGame.height };
+    while (!WindowShouldClose())
+    {
+        ballPosition = GetMousePosition();
+        mousePoint = GetMousePosition();
+
+        BeginDrawing();
+        DrawText("press ESC to go back", 75, 915, 20, RED);
+        DrawTexture(exitToGame, 1635, 925, WHITE);
+        DrawRectangleLines(75, 85, 1580, 820, ORANGE);
+        DrawCircleV(ballPosition, 10, BLACK);
+        HideCursor();
+        DrawFPS(75, 50);
+        DrawText("There were times when people decided to get better,", 100, 110, 50, YELLOW);
+        DrawText("to change themselfest to a better reality and a ", 100, 160, 50, YELLOW);
+        DrawText("different lifestyle, so that's why they all gathered  ", 100, 210, 50, YELLOW);
+        DrawText("all the trash on the planet together and put them on", 100, 260, 50, YELLOW);
+        DrawText(" a big recycling ship. But the big ship crashed on an ", 100, 310, 50, YELLOW);
+        DrawText("ice berg and all the garbage went floating in the ocean.", 100, 360, 50, YELLOW);
+        DrawText("Your task is to gather all the left overs of the trash ", 100, 410, 50, YELLOW);
+        DrawText("by completing chemistry and biology problems and ", 100, 460, 50, YELLOW);
+        DrawText("questions. Good luck saving the whole planet ECO pirate.", 100, 510, 50, YELLOW);
+        DrawText("*hint*", 100, 700, 50, RED);
+        DrawText("You should be very careful when choosing the right answer", 100, 770, 50, GREEN);
+        DrawText("because you have 3 lifes only.", 100, 820, 50, GREEN);
+        ClearBackground(BLUE);
+        EndDrawing();
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mousePoint, exitRulesButton))
+            game();
+
+    }
 }
 float HEIGHT, WIDTH;
 int main()
