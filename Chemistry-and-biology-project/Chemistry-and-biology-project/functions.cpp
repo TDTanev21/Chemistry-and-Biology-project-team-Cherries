f5d6e9590f6b1fc2b583cd133cf369d357400550
TDTanev21 <TDTanev21@codingburgas.bg>
2023-11-10 21:38:02 +0200

    Organize the variables and make a questions bank

--- Chemistry-and-biology-project/Chemistry-and-biology-project/functions.cpp
+++ Chemistry-and-biology-project/Chemistry-and-biology-project/functions.cpp
@@ -1,33 +1,63 @@
 #include "main.h"
 
+//variables for game()
+Texture2D trash1 = LoadTexture("../images/bottle.png");
+Texture2D trash2 = LoadTexture("../images/plasticBag.png");
+Texture2D trash3 = LoadTexture("../images/trash.png");
+Texture2D trash4 = LoadTexture("../images/spray.png");
+Texture2D trash5 = LoadTexture("../images/foodTrash.png");
+Texture2D trash6 = LoadTexture("../images/sodaCan.png");
+Texture2D trash7 = LoadTexture("../images/tire.png");
+Texture2D trash8 = LoadTexture("../images/notebook.png");
+Texture2D ship = LoadTexture("../images/ship.png");
+Rectangle trash1Colission = { 300, 200, trash1.width, trash1.height };
+Rectangle trash2Colission = { 1500, 400, trash2.width, trash2.height };
+Rectangle trash3Colission = { 150, 670, trash3.width, trash3.height };
+Rectangle trash4Colission = { 350, 500, trash4.width, trash4.height };
+Rectangle trash5Colission = { 1400, 100, trash5.width, trash5.height };
+Rectangle trash6Colission = { 1700, 675, trash6.width, trash6.height };
+Rectangle trash7Colission = { 600, 50, trash7.width, trash7.height };
+Rectangle trash8Colission = { 1200, 175, trash8.width, trash8.height };
+Texture2D bg = LoadTexture("../images/gamebg.png");
+Rectangle bgRect[2] = { { 0, 0, bg.width, bg.height} , {0, -bg.height, bg.width, bg.height} };
+float speed = 2;
+double shipX = GetScreenHeight() / 2 + 245;
+double shipY = 650;
+Rectangle shipCollision = { shipX, shipY, ship.width, ship.height };
+
+
+
+//variables for mainMenu()
+bool exitGame = false;
+
+Texture2D background = LoadTexture("../images/menubg.png");
+Texture2D startGame = LoadTexture("../images/start.png");
+Texture2D rules = LoadTexture("../images/rules.png");
+Texture2D quitGame = LoadTexture("../images/quit.png");
+
+Rectangle startGameButton = { 820, 350, startGame.width, startGame.height };
+Rectangle rulesButton = { 820, 475, rules.width, rules.height };
+Rectangle quitGameButton = { 820, 600, quitGame.width, quitGame.height };
+
+
+//variables for rulesFunction()
+Texture2D exitToGame = LoadTexture("../images/rulesToGame.png");
+Texture2D rulesBackground = LoadTexture("../images/rulesbg.png");
+Rectangle exitRulesButton = { 1635, 925, exitToGame.width, exitToGame.height };
+
+
+//variables for question() and removeQuestion()
+string questionsArr[5] = { "1","2", "3","4","5" };
+string answers[5] = { "1", "2", "3", "4", "5" };
+int questionsLength = 5;
+string currentQuestion;
+
+
 void game()
 {
     SetExitKey(KEY_ESCAPE);
    
-    Texture2D trash1 = LoadTexture("../images/bottle.png");
-    Texture2D trash2 = LoadTexture("../images/plasticBag.png");
-    Texture2D trash3 = LoadTexture("../images/trash.png");
-    Texture2D trash4 = LoadTexture("../images/spray.png");
-    Texture2D trash5 = LoadTexture("../images/foodTrash.png");
-    Texture2D trash6 = LoadTexture("../images/sodaCan.png");
-    Texture2D trash7 = LoadTexture("../images/tire.png");
-    Texture2D trash8 = LoadTexture("../images/notebook.png");
-    Texture2D ship = LoadTexture("../images/ship.png");
-    Rectangle trash1Colission = { 300, 200, trash1.width, trash1.height };
-    Rectangle trash2Colission = { 1500, 400, trash2.width, trash2.height };
-    Rectangle trash3Colission = { 150, 670, trash3.width, trash3.height };
-    Rectangle trash4Colission = { 350, 500, trash4.width, trash4.height };
-    Rectangle trash5Colission = { 1400, 100, trash5.width, trash5.height };
-    Rectangle trash6Colission = { 1700, 675, trash6.width, trash6.height };
-    Rectangle trash7Colission = { 600, 50, trash7.width, trash7.height };
-    Rectangle trash8Colission = { 1200, 175, trash8.width, trash8.height };
-    ballPosition = { -100.0f, -100.0f };
-    Texture2D bg = LoadTexture("../images/gamebg.png");
-    Rectangle bgRect[2] = { { 0, 0, bg.width, bg.height} , {0, -bg.height, bg.width, bg.height} };
-    float speed = 2;
-    double shipX = GetScreenHeight() / 2 + 245;
-    double shipY = 650;
-    Rectangle shipCollision = { shipX, shipY, ship.width, ship.height };
+   
     while (!WindowShouldClose())
     {
         shipY += 0.5f;
@@ -88,20 +118,7 @@ void game()
 
 void mainMenu()
 {
-    Texture2D background, startGame, rules, quitGame;
-    ballPosition = { -100.0f, -100.0f };
-    bool exitGame = false;
-
-
-    background = LoadTexture("../images/menubg.png");
-    startGame = LoadTexture("../images/start.png");
-    rules = LoadTexture("../images/rules.png");
-    quitGame = LoadTexture("../images/quit.png");
-
-
-    Rectangle startGameButton = { 820, 350, startGame.width, startGame.height };
-    Rectangle rulesButton = { 820, 475, rules.width, rules.height };
-    Rectangle quitGameButton = { 820, 600, quitGame.width, quitGame.height };
+    
 
 
 
@@ -145,13 +162,7 @@ void rulesFunction()
     SetExitKey(KEY_ESCAPE);
 
     
-    Texture2D exitToGame;
-    Texture2D rulesBackground;
-
-    exitToGame = LoadTexture("../images/rulesToGame.png");
-    rulesBackground = LoadTexture("../images/rulesbg.png");
-
-    Rectangle exitRulesButton = { 1635, 925, exitToGame.width, exitToGame.height };
+    
 
 
     while (!WindowShouldClose())
@@ -190,4 +201,25 @@ void rulesFunction()
 void question()
 {
     
+    
+        random_device(rd);
+        uniform_int_distribution<int> dist(0, (questionsLength > 1) ? questionsLength - 1 : 1);
+        currentQuestion = questionsArr[dist(rd)];
+    
+}
+void removeQuestion()
+{
+    for (int i = 0; i < 14; i++)
+    {
+        if (questionsArr[i] == currentQuestion)
+        {
+            questionsLength -= 1;
+            for (int j = i; j < questionsLength; j++)
+            {
+                questionsArr[j] = questionsArr[j + 1];
+                answers[j] = answers[j + 1];
+            }
+
+        }
+    }
 }
